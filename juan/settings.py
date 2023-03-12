c65dfd3fb122add6cbe814ece1faab2fb6c6f3a3
cafetiere <asuresh@gocardless.com>
2023-03-12 22:35:01 +0000

    add migration for surveys model

--- juan/settings.py
+++ juan/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'surveys.apps.SurveyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
